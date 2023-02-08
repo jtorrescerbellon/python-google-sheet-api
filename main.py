71a0ba63f627bfadfe977a6a02c6290f1cb65919
Joseph Torres <j.torrescerbellon@gmail.com>
2023-02-08 15:57:06 -0300

    Add .env

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Librerias a importar
+import os
+from dotenv import load_dotenv
+
+from httplib2 import Http
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.auth.transport.requests import Request
+
+# Lectura archivo .env
+load_dotenv()
+spreadsheetId = os.getenv('SPREADSHEET_ID')
+
 # Funcion para conectarse con Google sheet API
 def spreadsheetApi():
     pass
@@ -7,4 +21,4 @@ def main():
     pass
 
 if __name__ == '__main__':
-    main()
+    print(spreadsheetId)
